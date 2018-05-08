130f9246868d61ce9f1c1518cf9b15c4fc8e1dfc
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-05-08 21:40:55 +0200

    Add dropzone files and basic uploading

---
+++ share/www/html/index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+
+    <link href="css/basic.css" type="text/css" rel="stylesheet" />
+    <link href="css/dropzone.css" type="text/css" rel="stylesheet" />
+    <script src="js/dropzone.js"></script>>
+
+</head>
+
+<body>
+
+<form action="upload.php" class="dropzone"></form>
+
+</body>
+
+</html>
