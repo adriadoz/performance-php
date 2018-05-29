defd6d9519271f5b3badd714304727aeeb371422
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-05-29 20:34:45 +0200

    MySQL change to admit given id

--- share/www/html/db/imagesDB.sql
+++ share/www/html/db/imagesDB.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `images_db`;
 USE `images_db`;
 
 CREATE TABLE IF NOT EXISTS `images` (
-  `image_id`  INT NOT NULL AUTO_INCREMENT,
+  `image_id`  VARCHAR(255) NOT NULL,
   `name`  VARCHAR(60) NOT NULL,
   `file_name` VARCHAR(60) NOT NULL,
   `description` VARCHAR(255) DEFAULT '',
