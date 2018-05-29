352199b49ea607a6ab54625e7f6e9d18b84b0062
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-05-29 23:53:47 +0200

    Define Controller, Sender and Receiver AMQP

---
+++ share/www/html/src/Application/Service/ReceiveAMQP.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: adria.velardos
+ * Date: 29/5/18
+ * Time: 23:52
+ */
+
+namespace Performance\ImageLoader\Application\Service;
+
+
+class ReceiveAMQP
+{
+
+}
---
+++ share/www/html/src/Application/Service/SendAMQP.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Performance\ImageLoader\Application\Service;
+
+final class SendAMQP
+{
+
+}
